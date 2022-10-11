880ebf425b5811695b83f1ded26cbf60b1d649d0
jeicher <johanneicher@gmail.com>
2022-10-11 16:46:20 +0200

    simplified config

--- client/config.py
+++ client/config.py
@@ -1,28 +1,8 @@
 """Flask configuration."""
-from os import environ, path
-from dotenv import load_dotenv
 
-basedir = path.abspath(path.dirname(__file__))
-load_dotenv(path.join(basedir, '.env'))
-
-
-class Config:
+class Default:
     """Base config."""
-    SECRET_KEY = environ.get('SECRET_KEY')
-    SESSION_COOKIE_NAME = environ.get('SESSION_COOKIE_NAME')
     STATIC_FOLDER = 'static'
     TEMPLATES_FOLDER = 'templates'
 
 
-class ProdConfig(Config):
-    FLASK_ENV = 'production'
-    DEBUG = False
-    TESTING = False
-    DATABASE_URI = environ.get('PROD_DATABASE_URI')
-
-
-class DevConfig(Config):
-    FLASK_ENV = 'development'
-    DEBUG = True
-    TESTING = True
-    DATABASE_URI = environ.get('DEV_DATABASE_URI')
--- client/explorer/__init__.py
+++ client/explorer/__init__.py
@@ -4,9 +4,13 @@ from . import routes
 
 
 def init_app():
-    """Construct core Flask application."""
-    app = Flask(__name__, instance_relative_config=False)
-    app.config.from_object('config.Config')
+    app = Flask(__name__, instance_relative_config=True)
+    # the defaults in config.py
+    app.config.from_object('config.Default')
+    # the current instance settings
+    app.config.from_pyfile('application.cfg', silent=True)
+    # additional env variables prefixed with `FLASK_`
+    app.config.from_prefixed_env()
 
     # register "blueprints"
     app.register_blueprint(routes.query_bp)
