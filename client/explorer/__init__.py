4fffca0d940f17be1b1e69624e0bf2ecdac4074c
jeicher <johanneicher@gmail.com>
2022-10-12 09:37:41 +0200

    even more simplifications to config

--- client/explorer/__init__.py
+++ client/explorer/__init__.py
@@ -7,8 +7,6 @@ def init_app():
     app = Flask(__name__, instance_relative_config=True)
     # the defaults in config.py
     app.config.from_object('config.Default')
-    # the current instance settings
-    app.config.from_envvar('NIXPKGS_APPLICATION_SETTINGS')
     # additional env variables prefixed with `FLASK_`
     app.config.from_prefixed_env()
 
