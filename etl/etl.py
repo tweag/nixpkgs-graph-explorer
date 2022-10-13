6a312d086d304c215fda9bb467e394ecd514bc4f
Zhihan Zhang <iris.zhihan@gmail.com>
2022-10-13 16:09:55 +0200

    Add etl process

---
+++ etl/etl.py
@@ -0,0 +1,76 @@
+import subprocess
+import pandas as pd
+import requests
+import pathlib
+from pprint import pprint
+from contextlib import closing, contextmanager
+
+import os
+
+from gremlin_python.process.anonymous_traversal import traversal
+from gremlin_python.driver.driver_remote_connection import DriverRemoteConnection
+from gremlin_python.process.graph_traversal import __
+
+
+@contextmanager
+def gremlin_remote_connection(url: str):
+    r = DriverRemoteConnection(url,"g")
+    yield r
+    r.close()
+
+def extract_from_nix(output):    
+    nixpkgs_graph_nix_file_path = (
+        pathlib.Path(__file__).parent.joinpath("nixpkgs-graph.nix").resolve()
+    )
+    print("Downloading data from nix as a dataframe...")
+    output_file = open(output, "w")
+    with closing(output_file) as f:
+        subprocess.run(
+            args=['NIXPKGS_FLAKE_REF="github:nixos/nixpkgs/master" nix eval --json --file '+ str(nixpkgs_graph_nix_file_path) + ' --extra-experimental-features "nix-command flakes"'],
+            stdout=f,
+            shell=True
+        )
+    dataframe = pd.read_json(output)
+    return dataframe
+
+def gremlin_queries(dataframe0):
+    # have a small subset of dataframe
+    dataframe = dataframe0[:14830]
+    with gremlin_remote_connection('ws://localhost:8182/gremlin') as remote:
+        g = traversal().withRemote(remote)
+        # Remove nodes and edges
+        g.V().drop().iterate()
+        g.E().drop().iterate()
+
+        # Add nodes
+        print("Adding nodes...")
+        for _, row in dataframe.iterrows():
+            # TODO: there's some outputPath = None
+            g.addV(row["path"]).property("outputPath", str(row["outputPath"])).iterate()
+            print(str(_) + " node(s) added", end='\r')
+    
+        # Add edges
+        print("Adding edges...")
+        for _, row in dataframe.iterrows():
+            if row["outputPath"] != None:
+                for target in row["buildInputs"]:
+                    g.V().has("outputPath", row["outputPath"]).addE("buildInputs").to(__.V().has("outputPath", target)).property("label", "buildInputs").iterate()
+
+        print("Querying nodes...")
+        pprint(g.V().elementMap().toList())
+
+        print("Querying edges...")
+        pprint(g.E().valueMap().limit(5).toList())
+        print(g.V().count().toList())
+        print("Done.")
+
+def main():
+    # Extract data from nix as a dataframe
+    filepath = pathlib.Path(__file__).parent.joinpath("nodes.json").resolve()
+    df = extract_from_nix(filepath) if not os.path.exists(filepath) else pd.read_json(filepath)
+    
+    # Load data to database via sqlg and query the data
+    gremlin_queries(df)
+
+if __name__ == "__main__":
+    main()
---
+++ etl/nixpkgs-graph.nix
@@ -0,0 +1,54 @@
+# usage:
+# NIXPKGS_FLAKE_REF="github:nixos/nixpkgs/master" nix eval --json --file "./nixpkgs-graph.nix"
+
+let
+  nixpkgsFlakeRef = builtins.getEnv "NIXPKGS_FLAKE_REF";
+  pkgs = import (builtins.getFlake nixpkgsFlakeRef) { };
+in
+
+with pkgs.lib;
+
+let
+  recurse =
+    parentPath: name: value:
+    let
+      path = (if parentPath == "" then "" else parentPath + ".") + name;
+      valueEvalResult = builtins.tryEval value;
+    in
+    if valueEvalResult.success then
+      let okValue = valueEvalResult.value;
+      in
+      if isDerivation okValue then
+        {
+          inherit path;
+          # can't name it `outPath` because serialization would only output it instead of dict
+          # see Nix `toString` docs
+          outputPath =
+            let
+              pEvalResult = builtins.tryEval (toString okValue);
+            in
+            if pEvalResult.success then pEvalResult.value
+            else null;
+          buildInputs =
+            map
+              (p:
+                let
+                  pEvalResult = builtins.tryEval (toString p);
+                in
+                if pEvalResult.success then pEvalResult.value
+                else null
+              )
+              (okValue.buildInputs or [ ]);
+        }
+      else
+        if isAttrs okValue && (okValue.recurseForDerivations or false) then
+          mapAttrs (recurse path) okValue
+        else null
+    else null;
+
+in
+
+(collect
+  (x: x ? outputPath)
+  (mapAttrs (recurse "") pkgs)
+)
