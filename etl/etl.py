88f48cbf270690d3a9d82c02a621736175388820
Zhihan Zhang <iris.zhihan@gmail.com>
2023-02-14 11:50:16 +0100

    Apply formatter black

--- etl/etl.py
+++ etl/etl.py
@@ -8,29 +8,39 @@ from gremlin_python.process.anonymous_traversal import traversal
 from gremlin_python.driver.driver_remote_connection import DriverRemoteConnection
 from gremlin_python.process.graph_traversal import __
 
-def extract_from_nix():    
+
+def extract_from_nix():
     nixpkgs_graph_nix_file_path = (
         pathlib.Path(__file__).parent.joinpath("nixpkgs-graph.nix").resolve()
     )
-    print("Downloading data from nix as a dataframe...")    
-    result = subprocess.run('NIXPKGS_FLAKE_REF="github:nixos/nixpkgs/master" nix eval --json --file '+ str(nixpkgs_graph_nix_file_path) + ' --extra-experimental-features "nix-command flakes"', shell=True, stdout=subprocess.PIPE)
+    print("Downloading data from nix as a dataframe...")
+    result = subprocess.run(
+        'NIXPKGS_FLAKE_REF="github:nixos/nixpkgs/master" nix eval --json --file '
+        + str(nixpkgs_graph_nix_file_path)
+        + ' --extra-experimental-features "nix-command flakes"',
+        shell=True,
+        stdout=subprocess.PIPE,
+    )
     dataframe = pd.read_json(result.stdout.decode(), orient="records")
     return dataframe
 
+
 def data_process(dataframe):
-    dataframe["path"] = dataframe.groupby(["outputPath"])["path"] \
-                                 .transform(lambda x : ", ".join(x))
+    dataframe["path"] = dataframe.groupby(["outputPath"])["path"].transform(
+        lambda x: ", ".join(x)
+    )
     dataframe = dataframe.loc[dataframe.astype(str).drop_duplicates().index]
     dataframe["path"] = dataframe["path"].fillna("")
     dataframe = dataframe.reset_index(drop=True)
     outputName = get_outputNames(dataframe)
-    dataframe["buildInputsName"] = \
-        dataframe["buildInputs"].apply((lambda x : \
-                                        path_to_name(x, outputName)))
-    dataframe["propagatedBuildInputsName"] = \
-        dataframe["propagatedBuildInputs"].apply((lambda x : \
-                                                  path_to_name(x, outputName)))
+    dataframe["buildInputsName"] = dataframe["buildInputs"].apply(
+        (lambda x: path_to_name(x, outputName))
+    )
+    dataframe["propagatedBuildInputsName"] = dataframe["propagatedBuildInputs"].apply(
+        (lambda x: path_to_name(x, outputName))
+    )
     return dataframe
+
 
 def get_outputNames(dataframe):
     outputNameSet = set()
@@ -38,17 +48,21 @@ def get_outputNames(dataframe):
         outputNameSet = outputNameSet.union(set(name))
     return outputNameSet
 
+
 def path_to_name(x: list, outputName):
     names = []
     for p in x:
         if p != None:
-            names.append("-".join(p.split("-")[1:-1]) \
-                         if p.split("-")[-1] in outputName \
-                         else p[44:] )
+            names.append(
+                "-".join(p.split("-")[1:-1])
+                if p.split("-")[-1] in outputName
+                else p[44:]
+            )
     return names
 
+
 def path_to_outputpath(dataframe, path: str, name: str):
-    df_name = dataframe.query('name == @name')
+    df_name = dataframe.query("name == @name")
     if df_name.empty:
         return "not found."
     for _, row in df_name.iterrows():
@@ -57,31 +71,31 @@ def path_to_outputpath(dataframe, path: str, name: str):
     else:
         return "not found."
 
+
 def unique_insert_node(g, row):
-    g.V().has("package", "outputPath", str(row["outputPath"])) \
-        .fold() \
-        .coalesce(__.unfold(), \
-                  __.addV("package") \
-                  .property("outputPath", str(row["outputPath"])) \
-                  .property("path", row["path"]) \
-                  .property("pname", row["pname"]) \
-                  .property("name", row["name"]) \
-                  .property("version", row["version"])) \
-        .iterate()
+    g.V().has("package", "outputPath", str(row["outputPath"])).fold().coalesce(
+        __.unfold(),
+        __.addV("package")
+        .property("outputPath", str(row["outputPath"]))
+        .property("path", row["path"])
+        .property("pname", row["pname"])
+        .property("name", row["name"])
+        .property("version", row["version"]),
+    ).iterate()
+
 
 def unique_insert_edge(g, row, target, label):
     if g.V().has("package", "outputPath", target).toList() != []:
-        g.V().has("package", "outputPath", row["outputPath"]) \
-             .as_("v") \
-             .V().has("package", "outputPath", target) \
-                 .coalesce(__.inE("dependsOn") \
-                           .where(__.outV().as_("v")), \
-                           __.addE("dependsOn").from_("v") \
-                           .property("label", label)) \
-                 .iterate()
+        g.V().has("package", "outputPath", row["outputPath"]).as_("v").V().has(
+            "package", "outputPath", target
+        ).coalesce(
+            __.inE("dependsOn").where(__.outV().as_("v")),
+            __.addE("dependsOn").from_("v").property("label", label),
+        ).iterate()
+
 
 def gremlin_queries(dataframe):
-    with closing(DriverRemoteConnection('ws://localhost:8182/gremlin', "g")) as remote:
+    with closing(DriverRemoteConnection("ws://localhost:8182/gremlin", "g")) as remote:
         g = traversal().withRemote(remote)
         # Remove nodes and edges
         print("Initiating... (removing nodes and edges)")
@@ -93,27 +107,31 @@ def gremlin_queries(dataframe):
         for _, row in dataframe.iterrows():
             # TODO: there's some outputPath = None
             unique_insert_node(g, row)
-            print(str(_) + " node(s) added", end='\r')
-        print('\n')
+            print(str(_) + " node(s) added", end="\r")
+        print("\n")
 
         # Add edges
         print("Adding edges...")
         for _, row in dataframe.iterrows():
             if row["outputPath"] != None:
                 for i in range(len(row["buildInputs"])):
-                    target_outputpath = path_to_outputpath(dataframe, \
-                                                           row["buildInputs"][i], \
-                                                           row["buildInputsName"][i])
-                    if target_outputpath != 'not found.':
+                    target_outputpath = path_to_outputpath(
+                        dataframe, row["buildInputs"][i], row["buildInputsName"][i]
+                    )
+                    if target_outputpath != "not found.":
                         unique_insert_edge(g, row, target_outputpath, "buildInputs")
                 for i in range(len(row["propagatedBuildInputs"])):
-                    target_outputpath = path_to_outputpath(dataframe, \
-                                                           row["propagatedBuildInputs"][i], \
-                                                           row["propagatedBuildInputsName"][i])
-                    if target_outputpath != 'not found.':
-                        unique_insert_edge(g, row, target_outputpath, "propagatedBuildInputs")
-            print("Adding edges started from the " + str(_) + "(th) node", end='\r')
-        print('\n')
+                    target_outputpath = path_to_outputpath(
+                        dataframe,
+                        row["propagatedBuildInputs"][i],
+                        row["propagatedBuildInputsName"][i],
+                    )
+                    if target_outputpath != "not found.":
+                        unique_insert_edge(
+                            g, row, target_outputpath, "propagatedBuildInputs"
+                        )
+            print("Adding edges started from the " + str(_) + "(th) node", end="\r")
+        print("\n")
 
         # Query number of nodes
         print("Querying number of nodes...")
@@ -125,6 +143,7 @@ def gremlin_queries(dataframe):
 
         print("Done.")
 
+
 def main():
     # Extract data from nix as a dataframe
     df = extract_from_nix()
@@ -132,6 +151,7 @@ def main():
     df = data_process(df)
     # Load data to database via sqlg and query the data
     gremlin_queries(df)
+
 
 if __name__ == "__main__":
     main()
