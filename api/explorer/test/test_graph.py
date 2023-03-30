3fe1546e82a94960ad5c0d077343c799cfc5aa32
Dorran Howell <dorran.howell@tweag.io>
2023-03-30 15:17:30 +0200

    Add unit tests for insert_unique_directed_edge

--- api/explorer/test/test_graph.py
+++ api/explorer/test/test_graph.py
@@ -1,3 +1,4 @@
+from typing import Any, Mapping, cast
 import pytest
 from gremlin_python.driver import serializer
 from gremlin_python.driver.driver_remote_connection import DriverRemoteConnection
@@ -33,6 +34,29 @@ class DummyUniqueGraphElement(UniqueGraphElement):
 
     def get_id(self) -> str:
         return ElementId(self.unique_property)
+
+
+class DummyEdge(GraphElement):
+    """
+    Dummy edge element for testing purposes
+    """
+
+    prop1: str
+    prop2: int
+
+    @classmethod
+    def label(cls) -> str:
+        return "dummy-edge"
+
+
+class DummyEdgeNoProps(GraphElement):
+    """
+    Dummy edge element for testing purposes which has no properties
+    """
+
+    @classmethod
+    def label(cls) -> str:
+        return "dummy-edge-no-props"
 
 
 @pytest.fixture
@@ -90,3 +114,104 @@ def test_unit_insert_unique_vertex_does_not_duplicate_vertex(
     insert_unique_vertex(element, g)
     insert_unique_vertex(element, g)
     assert g.V().count().to_list() == [1]
+
+
+def test_unit_insert_unique_directed_edge_both_vertices_exist(
+    graph_connection: DriverRemoteConnection,
+):
+    from explorer.graph import insert_unique_directed_edge, insert_unique_vertex
+
+    # Pre-populate the graph with vertices
+    g = traversal().withRemote(graph_connection)
+    vertex1 = DummyUniqueGraphElement(unique_property="some-unique-value-1")
+    vertex2 = DummyUniqueGraphElement(unique_property="some-unique-value-2")
+    insert_unique_vertex(vertex1, g)
+    insert_unique_vertex(vertex2, g)
+
+    # Create the edge
+    insert_unique_directed_edge(
+        DummyEdgeNoProps(), from_vertex=vertex1, to_vertex=vertex2, g=g
+    )
+    assert g.E().count().to_list() == [1]
+
+
+def test_unit_insert_unique_directed_edge_does_nothing_if_from_vertex_not_found(
+    graph_connection: DriverRemoteConnection,
+):
+    from explorer.graph import insert_unique_directed_edge, insert_unique_vertex
+
+    # Pre-populate the graph with a single vertex. The other one is "missing".
+    g = traversal().withRemote(graph_connection)
+    vertex = DummyUniqueGraphElement(unique_property="some-unique-value-1")
+    missing_vertex = DummyUniqueGraphElement(unique_property="some-unique-value-2")
+    insert_unique_vertex(vertex, g)
+
+    # Create the edge
+    insert_unique_directed_edge(
+        DummyEdgeNoProps(), from_vertex=missing_vertex, to_vertex=vertex, g=g
+    )
+    assert g.E().count().to_list() == [0]
+
+
+def test_unit_insert_unique_directed_edge_does_nothing_if_to_vertex_not_found(
+    graph_connection: DriverRemoteConnection,
+):
+    from explorer.graph import insert_unique_directed_edge, insert_unique_vertex
+
+    # Pre-populate the graph with a single vertex. The other one is "missing".
+    g = traversal().withRemote(graph_connection)
+    vertex = DummyUniqueGraphElement(unique_property="some-unique-value-1")
+    missing_vertex = DummyUniqueGraphElement(unique_property="some-unique-value-2")
+    insert_unique_vertex(vertex, g)
+
+    # Create the edge
+    insert_unique_directed_edge(
+        DummyEdgeNoProps(), from_vertex=vertex, to_vertex=missing_vertex, g=g
+    )
+    assert g.E().count().to_list() == [0]
+
+
+def test_unit_insert_unique_directed_edge_does_not_create_duplicates(
+    graph_connection: DriverRemoteConnection,
+):
+    from explorer.graph import insert_unique_directed_edge, insert_unique_vertex
+
+    # Pre-populate the graph with vertices
+    g = traversal().withRemote(graph_connection)
+    vertex1 = DummyUniqueGraphElement(unique_property="some-unique-value-1")
+    vertex2 = DummyUniqueGraphElement(unique_property="some-unique-value-2")
+    insert_unique_vertex(vertex1, g)
+    insert_unique_vertex(vertex2, g)
+
+    # Create the edge
+    insert_unique_directed_edge(
+        DummyEdgeNoProps(), from_vertex=vertex1, to_vertex=vertex2, g=g
+    )
+    # Attempt to insert it again
+    insert_unique_directed_edge(
+        DummyEdgeNoProps(), from_vertex=vertex1, to_vertex=vertex2, g=g
+    )
+
+    assert g.E().count().to_list() == [1]
+
+
+def test_unit_insert_unique_directed_edge_created_edge_properties(
+    graph_connection: DriverRemoteConnection,
+):
+    from explorer.graph import insert_unique_directed_edge, insert_unique_vertex
+
+    # Pre-populate the graph with vertices
+    g = traversal().withRemote(graph_connection)
+    vertex1 = DummyUniqueGraphElement(unique_property="some-unique-value-1")
+    vertex2 = DummyUniqueGraphElement(unique_property="some-unique-value-2")
+    insert_unique_vertex(vertex1, g)
+    insert_unique_vertex(vertex2, g)
+
+    # Create the edge
+    edge = DummyEdge(prop1="foo", prop2=1)
+    insert_unique_directed_edge(edge, from_vertex=vertex1, to_vertex=vertex2, g=g)
+    # Attempt to read the edge's properties
+    element_maps = cast(
+        Mapping[Any, Any], g.E().hasLabel(edge.label()).element_map().to_list()
+    )
+    assert [DummyEdge.from_element_map(em) for em in element_maps] == [edge]
