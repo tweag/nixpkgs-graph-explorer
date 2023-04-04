726a941f7271d735469cc3b99b24d35eaaf05686
Dorran Howell <52341904+dorranh@users.noreply.github.com>
2023-04-04 10:41:46 +0200

    Add core package with data model and JSON schema (#56)

---
+++ core/explorer/core/__main__.py
@@ -0,0 +1,41 @@
+import click
+
+from explorer.core import model
+
+
+@click.command()
+@click.option(
+    "--out",
+    default="nixpkgs-graph.schema.json",
+    help="The output path at which to write the JSON Schema",
+    type=click.Path(),
+)
+@click.option(
+    "--stdout",
+    default=False,
+    help="Write outputs to stdout instead of to a file",
+    is_flag=True,
+)
+@click.option(
+    "--pretty/--no-pretty",
+    default=True,
+    help="Enable/disable pretty printing of the output JSON Schema",
+)
+def cli(out: str, stdout: bool, pretty: bool):
+    if pretty:
+        indent = 2
+    else:
+        indent = None
+
+    schema_json = model.NixGraph.schema_json(indent=indent)
+
+    if stdout:
+        print(schema_json)
+    else:
+        click.echo(f"Writing schema to {out}")
+        with open(out, "w") as f:
+            print(schema_json, file=f)
+        click.echo("Done.")
+
+
+cli()
