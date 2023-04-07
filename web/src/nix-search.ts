4a53e5c0a239107faee9c65638fdccb7485248f7
Guillaume Desforges <guillaume.desforges.pro@gmail.com>
2023-04-07 12:22:45 +0000

    format

--- web/src/nix-search.ts
+++ web/src/nix-search.ts
@@ -1,5 +1,5 @@
-import {LitElement, css, html} from "lit";
-import {customElement, state} from "lit/decorators.js";
+import { LitElement, css, html } from "lit";
+import { customElement, state } from "lit/decorators.js";
 import "@shoelace-style/shoelace/dist/themes/light.css";
 import "@shoelace-style/shoelace/dist/components/button/button.js";
 import "@shoelace-style/shoelace/dist/components/menu/menu.js";
@@ -8,15 +8,15 @@ import "@shoelace-style/shoelace/dist/components/input/input.js";
 import "@shoelace-style/shoelace/dist/components/spinner/spinner.js";
 import "@shoelace-style/shoelace/dist/components/alert/alert.js";
 import "@shoelace-style/shoelace/dist/components/icon/icon.js";
-import {classMap} from "lit/directives/class-map.js";
-import {getPackages} from "./api";
-import type {Cursor, Pkg} from "./api";
+import { classMap } from "lit/directives/class-map.js";
+import { getPackages } from "./api";
+import type { Cursor, Pkg } from "./api";
 
 export interface ClickItemPayload {
   name: string;
 }
 
-type EventInput = Event & {target: HTMLInputElement};
+type EventInput = Event & { target: HTMLInputElement };
 
 // TODO Add pagination
 @customElement("nix-search")
@@ -44,7 +44,7 @@ export class NixSearch extends LitElement {
   }
 
   async getPackages(search = "", limit = 10) {
-    const {packages, new_cursor} = await getPackages({search, limit});
+    const { packages, new_cursor } = await getPackages({ search, limit });
     this.pkgs = packages;
     this.cursor = new_cursor;
     this.loading = false;
@@ -70,12 +70,11 @@ export class NixSearch extends LitElement {
       >
       </sl-input>
       ${this.loading
-        ? html`
-        <div style="margin-top: 3em; width: 100%; text-align: center;">
-          <sl-spinner
-            style="font-size: 50px; --track-width: 10px;"
-          ></sl-spinner>
-        </div>`
+        ? html` <div style="margin-top: 3em; width: 100%; text-align: center;">
+            <sl-spinner
+              style="font-size: 50px; --track-width: 10px;"
+            ></sl-spinner>
+          </div>`
         : this.renderTable()}
     `;
   }
@@ -89,16 +88,16 @@ export class NixSearch extends LitElement {
     return html`
       <sl-menu>
         ${this.pkgs.map(
-      ({pname}) =>
-        html`
+          ({ pname }) =>
+            html`
               <sl-menu-item
                 @click=${this.clickPackageHandler}
                 value=${pname}
-                class=${classMap({selected: this.selectedPkg === pname})}
+                class=${classMap({ selected: this.selectedPkg === pname })}
                 >${pname}</sl-menu-item
               >
             `
-    )}
+        )}
       </sl-menu>
     `;
   }
@@ -109,7 +108,7 @@ export class NixSearch extends LitElement {
 
     if (name) {
       const options = {
-        detail: {name},
+        detail: { name },
         bubbles: true,
         composed: true,
       };
