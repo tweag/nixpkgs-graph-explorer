58fca6a66293ba2463f08b1d21118cba0d4be656
Dorran Howell <dorran.howell@tweag.io>
2023-05-17 13:47:26 +0200

    Add function for listing unique package pnames

--- api/explorer/api/queries/packages.py
+++ api/explorer/api/queries/packages.py
@@ -11,18 +11,110 @@ from explorer.api.queries.pagination import Cursor, CursorDirection
 
 logger = logging.getLogger(__name__)
 
+# Since the provided search predicate filters by name, we need to know the name
+# of the relevant vertex property. There might be smarter ways to extract this
+# rather than hard-coding it here.
+NAME_PROPERTY = "pname"
 
-# @dataclass
+
 class ListPackagesRequest(BaseModel):
     cursor: Optional[Cursor] = None
     search_predicate: Optional[str] = None
     limit: int = 10
 
 
-# @dataclass
 class ListPackagesResponse(BaseModel):
     new_cursor: Optional[Cursor]
     packages: list[Package]
+
+
+class ListUniquePackageNamesRequest(BaseModel):
+    cursor: Optional[Cursor] = None
+    search_predicate: Optional[str] = None
+    limit: int = 10
+
+
+class ListUniquePackageNamesResponse(BaseModel):
+    new_cursor: Optional[Cursor]
+    package_names: list[str]
+
+
+def list_unique_package_names(
+    request: ListPackagesRequest, conn: DriverRemoteConnection
+) -> ListPackagesResponse:
+    """Lists the unique names of packages available in the specified Gremlin Server
+
+    Args:
+        request (ListUniquePackageNamesRequest): The request parameters
+        conn (DriverRemoteConnection): A Gremlin Driver remote connection
+
+    Returns:
+        ListUniquePackageNamesResponse: A new cursor (if available) and the
+        list of package names matching the provided request.
+    """
+
+    # Connect to Gremlin Server and start a new traversal
+    g = traversal().withRemote(conn)
+
+    # First filter traversal based on predicate if one exists
+    filtered_traversal = g.V().values(NAME_PROPERTY)
+    if request.search_predicate:
+        logger.debug("Filtering traversal using provided predicate.")
+        filtered_traversal = filtered_traversal.is_(
+            TextP.startingWith(request.search_predicate)
+        )
+
+    # Apply ordering to the traversal
+    if not request.cursor or request.cursor.direction is CursorDirection.NEXT:
+        ordering = Order.asc  # type: ignore
+    else:
+        ordering = Order.desc  # type: ignore
+    ordered_filtered_traversal = filtered_traversal.order().by(ordering)
+
+    # Then we can filter based on the cursor, if one exists.
+    if request.cursor:
+        if request.cursor.direction == CursorDirection.NEXT:
+            logger.debug("cursor direction=next")
+            cursor_predicate = TextP.gte(request.cursor.row_id)
+        else:
+            logger.debug("cursor direction=previous")
+            cursor_predicate = TextP.lte(request.cursor.row_id)
+        page_traversal = (
+            ordered_filtered_traversal.is_(cursor_predicate)
+            .dedup()
+            .limit(request.limit + 1)
+        )
+    else:
+        logger.debug("Querying without cursor.")
+        # Note: Explicitly not sorting the results here since ordering steps in Gremlin
+        # can sometimes require that the server reads the entire traversal which can be
+        # inefficient.
+        page_traversal = ordered_filtered_traversal.dedup().limit(request.limit + 1)
+
+    # Execute traversal and parse results
+    package_names = page_traversal.to_list()
+
+    if not package_names:
+        return ListUniquePackageNamesResponse(new_cursor=None, package_names=[])
+
+    # If result set has same length as limit, we have reached the end of the result set
+    # and do not need to return a new cursor.
+    if len(package_names) <= request.limit:
+        return ListUniquePackageNamesResponse(
+            new_cursor=None, package_names=package_names
+        )
+
+    # Otherwise, construct the new cursor from the last item in the query result and
+    # return all other items.
+    new_cursor_direction = (
+        CursorDirection.NEXT if not request.cursor else request.cursor.direction
+    )
+    new_cursor = Cursor(row_id=package_names[-1], direction=new_cursor_direction)
+    # Note: Ignoring last item in result set since it is only used for constructing
+    # our new cursor
+    return ListUniquePackageNamesResponse(
+        new_cursor=new_cursor, package_names=package_names[:-1]
+    )
 
 
 def list_packages(
--- api/explorer/api/test/test_packages.py
+++ api/explorer/api/test/test_packages.py
@@ -13,6 +13,11 @@ PACKAGE_AA = Package(pname="package-aa", outputPath="aa/aa/aa")
 
 DUMMY_PACKAGES = [PACKAGE_A, PACKAGE_B, PACKAGE_C, PACKAGE_AA]
 
+# Packages with the same pname but different outputPaths
+PACKAGE_X = Package(pname="package-x", outputPath="path/to/x")
+PACKAGE_X_DUPLICATE = Package(pname="package-x", outputPath="path/to/a/different/x")
+DUPLICATE_PACKAGES = [PACKAGE_X, PACKAGE_X_DUPLICATE]
+
 
 @pytest.fixture
 def graph_connection():
@@ -23,6 +28,24 @@ def graph_connection():
     g = traversal().with_remote(conn)
     # Insert test dataset
     for p in DUMMY_PACKAGES:
+        insert_unique_vertex(p, g)
+    yield conn
+    # Dropping all data from graph on teardown
+    g.V().drop().iterate()
+    g.E().drop().iterate()
+    conn.close()
+
+
+@pytest.fixture
+def graph_connection_with_duplicates():
+    """Fixture providing a connection a graph which contains packages with duplicate pname values"""
+    # FIXME: use a traversal_source purely dedicated to tests
+    conn = default_remote_connection(
+        "ws://localhost:8182/gremlin", traversal_source="g"
+    )
+    g = traversal().with_remote(conn)
+    # Insert test dataset
+    for p in DUPLICATE_PACKAGES:
         insert_unique_vertex(p, g)
     yield conn
     # Dropping all data from graph on teardown
@@ -150,6 +173,173 @@ def test_unit_page_size_gt_total(graph_connection):
             PACKAGE_AA,
             PACKAGE_B,
             PACKAGE_C,
+        ],
+    )
+    assert response == expected_response
+
+
+def test_unit_package_names_no_duplicates(graph_connection_with_duplicates):
+    """Check that list_unique_package_names does not return duplicate values"""
+    request = packages.ListUniquePackageNamesRequest()
+    response = packages.list_unique_package_names(
+        request, graph_connection_with_duplicates
+    )
+    expected_response = packages.ListUniquePackageNamesResponse(
+        new_cursor=None,
+        package_names=[PACKAGE_X.pname],
+    )
+    assert response == expected_response
+
+
+def test_unit_package_names_filter_multi_matches(graph_connection):
+    """
+    Check that list_unique_package_names returns the expected response
+    when the provided search predicate matches multiple packages
+    """
+    cursor = None
+    search_predicate = "package-a"
+    request = packages.ListUniquePackageNamesRequest(
+        cursor=cursor, search_predicate=search_predicate
+    )
+    response = packages.list_unique_package_names(request, graph_connection)
+    expected_response = packages.ListUniquePackageNamesResponse(
+        new_cursor=None,
+        package_names=[PACKAGE_A.pname, PACKAGE_AA.pname],
+    )
+    assert response == expected_response
+
+
+def test_unit_package_names_filter_single_match(graph_connection):
+    """
+    Check that list_unique_package_names returns the expected response
+    when the provided search predicate matches a single package
+    """
+    cursor = None
+    search_predicate = "package-b"
+    request = packages.ListUniquePackageNamesRequest(
+        cursor=cursor, search_predicate=search_predicate
+    )
+    response = packages.list_unique_package_names(request, graph_connection)
+    expected_response = packages.ListUniquePackageNamesResponse(
+        new_cursor=None,
+        package_names=[PACKAGE_B.pname],
+    )
+    assert response == expected_response
+
+
+def test_unit_package_names_filter_no_match(graph_connection):
+    """
+    Check that list_unique_package_names returns the expected response
+    when the provided search predicate does not match any packages
+    """
+    cursor = None
+    search_predicate = "does-not-exist"
+    request = packages.ListUniquePackageNamesRequest(
+        cursor=cursor, search_predicate=search_predicate
+    )
+    response = packages.list_unique_package_names(request, graph_connection)
+    expected_response = packages.ListUniquePackageNamesResponse(
+        new_cursor=None,
+        package_names=[],
+    )
+    assert response == expected_response
+
+
+def test_unit_package_names_cursor_direction_previous(graph_connection):
+    """
+    Check that list_unique_package_names returns the expected response
+    when a cursor in the PREVIOUS direction is provided
+    """
+    cursor = Cursor(row_id=PACKAGE_B.pname, direction=CursorDirection.PREVIOUS)
+    search_predicate = None
+    request = packages.ListUniquePackageNamesRequest(
+        cursor=cursor, search_predicate=search_predicate, limit=2
+    )
+    response = packages.list_unique_package_names(request, graph_connection)
+    expected_response = packages.ListUniquePackageNamesResponse(
+        new_cursor=Cursor(row_id=PACKAGE_A.pname, direction=CursorDirection.PREVIOUS),
+        package_names=[PACKAGE_B.pname, PACKAGE_AA.pname],
+    )
+    assert response == expected_response
+
+
+def test_unit_package_names_cursor_direction_next(graph_connection):
+    """
+    Check that list_unique_package_names returns the expected response
+    when a cursor in the NEXT direction is provided
+    """
+    cursor = Cursor(row_id=PACKAGE_B.pname, direction=CursorDirection.NEXT)
+    search_predicate = None
+    request = packages.ListUniquePackageNamesRequest(
+        cursor=cursor, search_predicate=search_predicate, limit=2
+    )
+    response = packages.list_unique_package_names(request, graph_connection)
+    expected_response = packages.ListUniquePackageNamesResponse(
+        new_cursor=None,
+        package_names=[PACKAGE_B.pname, PACKAGE_C.pname],
+    )
+    assert response == expected_response
+
+
+def test_unit_package_names_page_size_lt_total(graph_connection):
+    """
+    Check that list_unique_package_names returns the expected response
+    when limit is less than the length of the total result set
+    """
+    cursor = None
+    search_predicate = None
+    request = packages.ListUniquePackageNamesRequest(
+        cursor=cursor, search_predicate=search_predicate, limit=2
+    )
+    response = packages.list_unique_package_names(request, graph_connection)
+    expected_response = packages.ListUniquePackageNamesResponse(
+        new_cursor=Cursor(row_id=PACKAGE_B.pname),
+        package_names=[PACKAGE_A.pname, PACKAGE_AA.pname],
+    )
+    assert response == expected_response
+
+
+def test_unit_package_names_page_size_eq_total(graph_connection):
+    """
+    Check that list_unique_package_names returns the expected response
+    when limit is equal to the length of the total result set
+    """
+    cursor = None
+    search_predicate = None
+    request = packages.ListUniquePackageNamesRequest(
+        cursor=cursor, search_predicate=search_predicate, limit=4
+    )
+    response = packages.list_unique_package_names(request, graph_connection)
+    expected_response = packages.ListUniquePackageNamesResponse(
+        new_cursor=None,
+        package_names=[
+            PACKAGE_A.pname,
+            PACKAGE_AA.pname,
+            PACKAGE_B.pname,
+            PACKAGE_C.pname,
+        ],
+    )
+    assert response == expected_response
+
+
+def test_unit_package_names_page_size_gt_total(graph_connection):
+    """
+    Check that list_unique_package_names returns the expected response
+    when limit larger than the length of the total result set
+    """
+    cursor = None
+    search_predicate = None
+    request = packages.ListUniquePackageNamesRequest(
+        cursor=cursor, search_predicate=search_predicate, limit=5
+    )
+    response = packages.list_unique_package_names(request, graph_connection)
+    expected_response = packages.ListUniquePackageNamesResponse(
+        new_cursor=None,
+        package_names=[
+            PACKAGE_A.pname,
+            PACKAGE_AA.pname,
+            PACKAGE_B.pname,
+            PACKAGE_C.pname,
         ],
     )
     assert response == expected_response
