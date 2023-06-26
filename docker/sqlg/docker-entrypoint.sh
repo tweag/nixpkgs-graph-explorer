12fb809a6e5c354cf4239a199f300544c8e3c3f4
Guillaume Desforges <guillaume.desforges.pro@gmail.com>
2023-06-26 16:03:10 +0200

    sqlg: no execution timeout

--- docker/sqlg/docker-entrypoint.sh
+++ docker/sqlg/docker-entrypoint.sh
@@ -18,4 +18,4 @@ fi
 IP=$(ip -o -4 addr list eth0 | perl -n -e 'if (m{inet\s([\d\.]+)\/\d+\s}xms) { print $1 }')
 sed -i "s|^host:.*|host: $IP|" $CONF_FILE
 
-/gremlin-server/bin/gremlin-server.sh console
+JAVA_OPTIONS="-Xmx2G" /gremlin-server/bin/gremlin-server.sh console
